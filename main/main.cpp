f9ae8d0752d5b77d2a97d95a99cbb8f20922e774
Alexandre Rollet <alexandre.rollet@edu.devinci.fr>
2026-03-23 14:16:11 +0100

    Départ projet

--- main/main.cpp
+++ main/main.cpp
@@ -1,16 +1,12 @@
 #include <iostream>
 using namespace std;
-#include "affichage.h"
 
 int main(void) {
     system("chcp 65001"); //permet d'afficher correctement les charactères ASCII
 
-    afficher_intro();
     string nom_personnage = "";
     cout << "Choisissez un nom de cowboy:" << endl;
     cin >> nom_personnage;
-    
-    afficher_carte();
 
     return 0;
 }
